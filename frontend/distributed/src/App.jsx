ff8ae43bbd696b849f5691d0c7aab589c72bbddd
ahttan <94210764+meimei2548@users.noreply.github.com>
2025-10-15 14:26:50 +0700

    ui login,signin and setup back

--- frontend/distributed/src/App.jsx
+++ frontend/distributed/src/App.jsx
@@ -11,11 +11,32 @@ import Login from "./pages/Auth/Login";
 import SignUp from "./pages/Auth/SignUp";
 import Home from "./pages/Dashboard/Home";
 import Income from "./pages/Dashboard/Income";
+import Expense from "./pages/Dashboard/Expense";
+
 const App = () => {
  return (
   <div>
-    <Router></Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Root/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signUp" element={<SignUp/>}/>
+        <Route path="/dashboard" element={<Home/>}/>
+        <Route path="/income" exact element={<Income/>}/>
+        <Route path="/expense" exact element={<Expense/>}/>
+      </Routes>
+    </Router>
   </div>
- )
-}
+ );
+};
 export default App;
+
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+   ) : (
+    <Navigate to="/login" />
+  )
+}
--- frontend/distributed/src/pages/Auth/SignUp.jsx
+++ frontend/distributed/src/pages/Auth/SignUp.jsx
@@ -1,10 +1,93 @@
-import React from "react";
+import React, {useState} from "react";
+import AuthLayout from "../../components/layout/AuthLayout";
+import { useNavigate, Link } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelect from "../../components/Inputs/ProfilePhotoSelect";
 
 const SignUp = () => {
+  const [profilePic , setProfilePic] = useState(null)
+  const [fullName, setFullName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  const [error, setError] = useState("")
+
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    let profileImageUrl =""; 
+
+    if(!fullName) {
+      setError("Please enter your name")
+      return;
+    }
+
+    if(!validateEmail(email)) {
+      setError("Please enter a valid email address")
+      return;
+    }
+
+    if(!password) {
+      setError("Please enter your passsword")
+      return;
+    }
+
+    setError("");
+  }
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+    <AuthLayout>
+      <div className="lg:w-[100%] h-auto md:h-full md:mt-0 flex flex-col justify-center" >
+        <h3 className="text-xl font-semibold text-black ">Create an Account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">Join us today by entering you detail below.</p>
+
+        <form onSubmit={handleSignUp}>
+
+          <ProfilePhotoSelect image={profilePic} setImage={setProfilePic}/>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <Input
+              value={fullName}
+              onChange={({target})=> setFullName(target.value)}
+              label="Full Name"
+              placeholder="Fern"
+              type="text"
+            />
+
+            <Input
+            value={email}
+            onChange={({target}) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="nongfern@narak.com"
+            type="text"
+            />
+            <div className="col-span-2">
+            <Input
+              value={password}
+              onChange={({target}) => setPassword(target.value)}
+              label="Password"
+              placeholder="........"
+              type="password"
+              autocomplete ="new-password"
+            />
+            {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+            
+            <button type="submit" className="btn-primary">SIGN Up</button>
+            
+            <p className="text-slate-800 mt-3">Already have an account?{" "}
+              <Link className="font-medium text-primary underline" to="/login">
+              Login
+              </Link>
+            </p>
+            </div>
+            
+          </div>
+
+        </form>
+      </div>
+    </AuthLayout>
   );
 };
 
